4458e6158bf1b295b6e5999423b22cf0e706f17f
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-07-18 19:07:44 -0400

    agregados datos iniciales

---
+++ Oracle SQL - Datos.sql
@@ -0,0 +1,67 @@
+----------------------------------------------------------------------------------------------------
+-- Tabla: Facturas
+----------------------------------------------------------------------------------------------------
+INSERT INTO alog_facturas (fecha) VALUES ('02/09/2019');
+INSERT INTO alog_facturas (fecha) VALUES ('05/06/2020');
+INSERT INTO alog_facturas (fecha) VALUES ('15/12/2019');
+INSERT INTO alog_facturas (fecha) VALUES ('19/04/2020');
+INSERT INTO alog_facturas (fecha) VALUES ('16/10/2019');
+INSERT INTO alog_facturas (fecha) VALUES ('08/10/2019');
+INSERT INTO alog_facturas (fecha) VALUES ('26/12/2019');
+INSERT INTO alog_facturas (fecha) VALUES ('28/07/2019');
+INSERT INTO alog_facturas (fecha) VALUES ('19/03/2020');
+INSERT INTO alog_facturas (fecha) VALUES ('17/07/2020');
+
+----------------------------------------------------------------------------------------------------
+-- Tabla: Items
+----------------------------------------------------------------------------------------------------
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Optimized discrete software', 83418.33, 4, 8);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Decentralized systematic focus group', 88217.9, 5, 4);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Multi-lateral 4th generation knowledge user', 50343.34, 6, 3);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Balanced optimal function', 12197.64, 20, 9);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Monitored incremental frame', 17578.36, 12, 9);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Ameliorated multi-state infrastructure', 98856.86, 5, 9);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Enhanced 6th generation database', 53809.12, 9, 5);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Grass-roots motivating methodology', 84600.55, 17, 10);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Grass-roots transitional methodology', 54190.44, 12, 3);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Intuitive tangible budgetary management', 67754.11, 15, 10);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Integrated encompassing matrix', 42981.21, 6, 3);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Configurable national support', 86539.32, 17, 7);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Adaptive modular policy', 49292.62, 7, 9);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Multi-channelled client-server info-mediaries', 52934.48, 10, 2);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Public-key 3rd generation extranet', 18768.15, 18, 6);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Triple-buffered encompassing software', 96828.09, 14, 1);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Virtual needs-based open system', 44019.13, 6, 9);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Balanced analyzing website', 52063.49, 9, 5);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Universal scalable open system', 80893.28, 18, 3);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Open-source dedicated architecture', 23209.29, 12, 6);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Front-line system-worthy instruction set', 86715.52, 19, 5);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Focused tangible pricing structure', 64776.34, 20, 10);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Multi-lateral clear-thinking approach', 86806.44, 4, 3);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Face to face asymmetric array', 7810.37, 9, 8);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Virtual zero tolerance instruction set', 58310.73, 12, 9);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Realigned intermediate matrices', 12173.61, 6, 6);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Open-architected impactful concept', 75752.27, 7, 8);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Virtual tertiary conglomeration', 61125.91, 7, 1);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Streamlined context-sensitive circuit', 21110.18, 14, 4);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Up-sized solution-oriented initiative', 6515.22, 16, 7);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Cross-group eco-centric utilisation', 59329.57, 18, 4);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Cross-platform dedicated throughput', 48497.9, 2, 2);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Decentralized logistical open system', 40216.39, 6, 4);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Phased national concept', 85519.67, 5, 10);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Open-architected neutral data-warehouse', 88970.96, 10, 5);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Distributed leading edge knowledge base', 57946.11, 1, 9);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Integrated value-added implementation', 3047.54, 19, 1);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Object-based high-level array', 72171.68, 14, 8);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Networked 4th generation encoding', 45594.69, 5, 1);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Cross-platform exuding leverage', 49815.73, 6, 6);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Virtual grid-enabled contingency', 46189.26, 3, 7);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Balanced national concept', 33584.13, 4, 1);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Upgradable value-added capability', 5055.0, 13, 3);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Intuitive disintermediate approach', 78188.54, 17, 8);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Up-sized 6th generation middleware', 54849.92, 1, 10);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Sharable eco-centric flexibility', 90122.68, 5, 5);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Team-oriented static extranet', 57066.52, 6, 2);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Fully-configurable executive data-warehouse', 28936.92, 18, 3);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Inverse 5th generation hierarchy', 94371.35, 2, 9);
+INSERT INTO alog_items (descripcion, precio_unitario, cantidad, factura_id) VALUES ('Phased dynamic installation', 97453.83, 10, 4);
