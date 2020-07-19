e156cf99c450816cfaeace93a9c438d07d3da074
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-07-18 22:44:03 -0400

    agregado el doPost y el método que maneja la eliminación de una factura

--- src/main/java/cl/rhacs/alog/controladores/FacturasController.java
+++ src/main/java/cl/rhacs/alog/controladores/FacturasController.java
@@ -1,6 +1,7 @@
 package cl.rhacs.alog.controladores;
 
 import java.io.IOException;
+import java.sql.Date;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,7 +19,7 @@ import cl.rhacs.alog.modelos.Item;
 import cl.rhacs.alog.repositorios.FacturasRepositorio;
 import cl.rhacs.alog.repositorios.ItemsRepositorio;
 
-@WebServlet(name = "FacturasController", urlPatterns = { "", "/facturas/*" })
+@WebServlet(name = "FacturasController", urlPatterns = { "", "/facturas/*", "/chauchera/*" })
 public class FacturasController extends HttpServlet {
 
     // Constantes
@@ -153,27 +154,137 @@ public class FacturasController extends HttpServlet {
         req.getRequestDispatcher("/WEB-INF/vistas/facturas/detalles.jsp").forward(req, res);
     }
 
-    // Solicitudes
+    // Solicitudes GET
     // -----------------------------------------------------------------------------------------
 
+    /**
+     * Maneja las Solicitudes GET
+     * 
+     * @param req objeto {@link HttpServletRequest} con la consulta que le hace el
+     *            cliente al {@link HttpServlet}
+     * @param res objeto {@link HttpServletResponse} con la respuesta que le envía
+     *            el {@link HttpServlet} al cliente
+     * @throws ServletException si un error interrumpe el normal funcionamiento del
+     *                          {@link HttpServlet}
+     * @throws IOException      si un error de entrada/salida ocurre mientras el
+     *                          {@link HttpServlet} intenta resolver la respuesta
+     */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("GET");
         // Obtener URI
         String uri = req.getRequestURI();
 
-        // Crear matcher para detalles
+        // Crear matchers
         Matcher detalles = Pattern.compile("\\/alog\\/facturas\\/([0-9]+)").matcher(uri);
+        Matcher eliminar = Pattern.compile("\\/alog\\/chauchera\\/([0-9]+)").matcher(uri);
 
         // Filtrar uri
         if (uri.equals("/alog/")) {
             this.mostrarListado(req, resp);
+        } else if (uri.equals("/alog/facturas/nueva")) {
+            // Mostrar formulario
+            req.getRequestDispatcher("/WEB-INF/vistas/facturas/formulario.jsp").forward(req, resp);
         } else if (detalles.matches()) {
             // Extraer identificador
             int id = Integer.parseInt(detalles.group(1));
 
             // Ejecutar método correspondiente
             this.mostrarDetalles(req, resp, id);
+        } else if (eliminar.matches()) {
+            // Obtener identificador
+            int id = Integer.parseInt(eliminar.group(1));
+
+            // Buscar factura
+            Factura factura = facturasRepositorio.buscarPorId(id);
+
+            // Verificar si existe
+            if (factura != null) {
+                // Buscar los items de la factura
+                List<Item> items = itemsRepositorio.buscarPorFacturaId(factura.getFacturaId());
+
+                // Verificar resultados
+                if (items != null) {
+                    // Eliminar los items
+                    items.forEach(item -> {
+                        itemsRepositorio.eliminarRegistro(item);
+                    });
+                }
+
+                // Borrar factura
+                facturasRepositorio.eliminarRegistro(factura);
+
+                // Redireccionar
+                resp.sendRedirect(req.getContextPath() + "/?rem=" + id);
+            } else {
+                // Redireccionar
+                resp.sendRedirect(req.getContextPath() + "/?noid=" + id);
+            }
         }
     }
-    
+
+    // Solicitudes POST
+    // -----------------------------------------------------------------------------------------
+
+    /**
+     * Maneja las Solicitudes POST
+     * 
+     * @param req objeto {@link HttpServletRequest} con la consulta que le hace el
+     *            cliente al {@link HttpServlet}
+     * @param res objeto {@link HttpServletResponse} con la respuesta que le envía
+     *            el {@link HttpServlet} al cliente
+     * @throws ServletException si un error interrumpe el normal funcionamiento del
+     *                          {@link HttpServlet}
+     * @throws IOException      si un error de entrada/salida ocurre mientras el
+     *                          {@link HttpServlet} intenta resolver la respuesta
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Obtener uri
+        String uri = req.getRequestURI();
+        System.out.println("POST");
+
+        // Verificar que la uri sea la correcta
+        if (uri.equals("/alog/facturas/nueva/")) {
+            // Obtener la fecha de la factura
+            Date fecha = Date.valueOf(req.getParameter("fecha"));
+
+            // Crear factura
+            Factura factura = new Factura();
+
+            // Poblar objeto
+            factura.setFecha(fecha);
+
+            // Agregar al repositorio
+            if (facturasRepositorio.agregarRegistro(factura)) {
+                // Obtener datos de la factura agregada
+                factura = facturasRepositorio.buscarUltima();
+
+                // Obtener los datos de los Items ingresados por el usuario
+                String[] cantidades = req.getParameterValues("cantidad");
+                String[] descripciones = req.getParameterValues("descripcion");
+                String[] precios = req.getParameterValues("precio");
+
+                // Crear los items
+                for (int i = 0; i < cantidades.length; i++) {
+                    // Convertir datos
+                    int cant = Integer.parseInt(cantidades[i]);
+                    double valor = Double.parseDouble(precios[i]);
+
+                    // Crear item
+                    Item item = new Item(-1, descripciones[i], valor, cant, factura.getFacturaId());
+
+                    // Agregar item al repositorio
+                    itemsRepositorio.agregarRegistro(item);
+                }
+
+                // Redireccionar al detalle de la factura
+                resp.sendRedirect(req.getContextPath() + "/facturas/" + factura.getFacturaId());
+            }
+        } else {
+            // Redireccionar al usuario
+            resp.sendRedirect(req.getContextPath());
+        }
+    }
+
 }
